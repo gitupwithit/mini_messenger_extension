df1eeee866fb1c98943ce446f39f557d945136be
Perffine <perfectly.fine.applications@gmail.com>
2024-02-06 12:15:58 -0500

    basic message reply input

--- extension/script.js
+++ extension/script.js
@@ -1,1 +1,13 @@
 console.log("script.js loaded")
+
+const outgoingMessageForm = document.getElementById('outgoingMessageForm');
+const messageToSend = document.getElementById('messageToSend');
+
+outgoingMessageForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    console.log('Form submitted');
+    event.target.querySelector('button[type="submit"]').disabled = true;
+    const data = { title: messageToSend.value }
+    console.log("message to send: ", data)
+
+})
