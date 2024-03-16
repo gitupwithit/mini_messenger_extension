89f13c67a496494abc2c9cbf3c0bf6a91fc54a98
Perffine <perfectly.fine.applications@gmail.com>
2024-03-16 19:15:14 -0400

    reworking flow

--- extension/script.js
+++ extension/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("script.js loaded")
 
 // When the side panel opens
@@ -34,8 +33,7 @@ document.getElementById('replyButton').addEventListener('click', function() {
 
 document.getElementById('okButton').addEventListener('click', function() {
     console.log("ok Button clicked")
-    // server to check for existence of user's partner
-    chrome.runtime.sendMessage({ action: "checkForPartner", event: data });
+    showMessages();
 });
 
 
@@ -70,12 +68,14 @@ function showChoosePartner() {
     document.getElementById('outgoingMessageContainer').style.display = 'none';
 }
 
-function showMessages() {
+function showMessages(userData) {
+    const data = 
+    chrome.runtime.sendMessage({ action: "getNewMessages", event: data });
     document.getElementById('signIn').style.display = 'none';
     document.getElementById('choosePartnerContainer').style.display = 'none';
     document.getElementById('incomingMessageContainer').style.display = 'block';
     document.getElementById('outgoingMessageContainer').style.display = 'block';
-    document.getElementById('statusMessage').style.display = 'block';
+    document.getElementById('statusMessage').style.display = 'none';
 }
 
 function hideMessages() {
@@ -87,7 +87,8 @@ function hideMessages() {
 
 //welcome back and user verify partner
 function welcomeUserBack(text) {
-    console.log(text)
+    console.log(text);
+    document.getElementById('signIn').style.display = 'none';
     document.getElementById('statusMessage').style.display = 'block';
     document.getElementById('okButton').style.display = 'block';
     if (text === "") {
@@ -99,9 +100,9 @@ function welcomeUserBack(text) {
 }
 
 chrome.runtime.onMessage.addListener((message, event, sender, sendResponse) => {
-    console.log("message:", message, "events", event)
-    if (message.action === "welcomeUserBack") {
-        let text = message.event.data
+    console.log("message:", message)
+    if (message.action === "welcomeBack") {
+        let text = message.event
         console.log("welcome user back")
         console.log(text)
         welcomeUserBack(text);
@@ -135,131 +136,3 @@ chrome.runtime.onMessage.addListener((message, event, sender, sendResponse) => {
         showMessages();
     }
 })
-=======
-console.log("script.js loaded")
-
-// When the side panel opens
-document.addEventListener('DOMContentLoaded', function() {
-    checkAuthentication();
-    console.log("dcom content loaded")
-    // Request message
-    chrome.runtime.sendMessage({ action: "fetchMessage" });
-});
-
-document.getElementById('signIn').addEventListener('click', function() {
-    console.log("sign in button clicked")
-    chrome.runtime.sendMessage({ action: "userSignIn" });
-});
-
-document.getElementById('choosePartnerButton').addEventListener('click', function() {
-    console.log("choose partner button clicked");
-    const data = document.getElementById('choosenPartner').value
-    if (data.includes("@")) {
-        document.getElementById('responseMessage').innerHTML = "must be a gmail address, without '@gmail.com'"
-    } else {
-        document.getElementById('responseMessage').innerHTML = ""
-        chrome.runtime.sendMessage({ action: "userChoosePartner", event: data });
-    }
-});
-
-document.getElementById('replyButton').addEventListener('click', function() {
-    console.log("reply button clicked")
-    const data = document.getElementById('messageToSend').value
-    console.log("user clicks message send button, message: ", data)
-    chrome.runtime.sendMessage({ action: "sendMessageToOtherUser", event: data });
-});
-
-document.getElementById('clearStatusMessageButton').addEventListener('click', function() {
-    console.log("clearStatusMessageButton clicked")
-    // server to check for existence of user's partner
-    chrome.runtime.sendMessage({ action: "checkForPartner", event: data });
-});
-
-
-function checkAuthentication() {
-    chrome.storage.local.get(['access_token'], function(result) {
-        if (result.access_token) {
-        // Ask the service worker to validate the token
-            chrome.runtime.sendMessage({ action: "validateToken", token: result.access_token }, function(response) {
-            if (response.isValid) {
-                // The token is valid, proceed to fetch calendar events
-                // console.log("token is valid, proceed to fetch calendar events")
-                document.getElementById('signIn').style.display = 'none';
-                
-            } else {
-                // Token is not valid, show the 'Authorize' button
-                console.log("Token is not valid, show the 'Authorize' button")
-                
-                }
-            });
-        } else {
-        // No token found, show the 'Authorize' button
-        console.log("Token not found, show the 'Authorize' button")
-        document.getElementById('signIn').style.display = 'block';
-        }
-    });
-}
-
-function showChoosePartner() {
-    document.getElementById('signIn').style.display = 'none';
-    document.getElementById('choosePartnerContainer').style.display = 'block';
-    document.getElementById('incomingMessageContainer').style.display = 'none';
-    document.getElementById('outgoingMessageContainer').style.display = 'none';
-}
-
-function showMessages() {
-    document.getElementById('signIn').style.display = 'none';
-    document.getElementById('choosePartnerContainer').style.display = 'none';
-    document.getElementById('incomingMessageContainer').style.display = 'block';
-    document.getElementById('outgoingMessageContainer').style.display = 'block';
-    document.getElementById('statusMessage').style.display = 'block';
-}
-
-function hideMessages() {
-    document.getElementById('signIn').style.display = 'block';
-    document.getElementById('choosePartnerContainer').style.display = 'none';
-    document.getElementById('incomingMessageContainer').style.display = 'none';
-    document.getElementById('outgoingMessageContainer').style.display = 'none';
-}
-
-function welcomeUserBack() {
-    document.getElementById('responseMessage').innerHTML = "Welcome Back!";
-    document.getElementById('statusMessage').style.display = 'block';
-}
-
-chrome.runtime.onMessage.addListener((message, events, sender, sendResponse) => {
-    console.log("message:", message, "events", events)
-    if (message.action === "welcomeUserBack") {
-        console.log("welcome user back")
-        welcomeUserBack();
-    };
-    if (message.action === "showChoosePartner") {
-        console.log("show choose partner now");
-        showChoosePartner();
-    }
-    if (message.action === "partnerIsInDb") {
-        console.log("partner is in db");
-        document.getElementById('responseMessage').innerHTML = "Your partner is registered too!";
-        showMessages();
-    }
-    if (message.action === "partnerIsNotInDb") {
-        console.log("partner is not in db");
-        document.getElementById('responseMessage').innerHTML = "Your partner is not registered yet :/";
-        showMessages();
-    }
-    if (message.action === "showMessages") {
-        console.log("show messages now");
-        showMessages();
-    }
-    if (message.action === "messageForUser") {
-        document.getElementById('messageFrom').innerHTML = "Message from: " + message.event.sender;
-        document.getElementById('incomingMessageText').innerHTML = message.event.messageText;
-        showMessages();
-    }
-    if (message.action === "messageSent") {
-        console.log("message sent notification")
-        document.getElementById('responseMessage').innerHTML = "Message sent!";
-        showMessages();
-    }
-})
->>>>>>> 5ccecd8d88ec9b841b77d6d1c1ff86a3cb06dc56
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log("service-worker.js loaded")
 
 const socket = new WebSocket('ws://localhost:8000');
@@ -59,11 +58,14 @@ function checkUser() {
     };
     socket.onmessage = function(wsEvent) {
         console.log(`Message from server: ${wsEvent.data}`);
+        const dataObject = JSON.parse(wsEvent.data);
+        console.log(`Message: ${dataObject.message}`);
         if (wsEvent.data === "userAdded") {
             chrome.runtime.sendMessage({ action: "showChoosePartner"});
         }
-        if (wsEvent.data === "welcomeUserBack") {
-            chrome.runtime.sendMessage({ action: "welcomeUserBack", event: wsEvent});
+        if (dataObject.instruction === "welcomeBack") {
+            console.log("this")
+            chrome.runtime.sendMessage({ action: "welcomeBack", event: dataObject.message}); 
         }
     };
 }
@@ -107,16 +109,17 @@ function checkPartner(partnerID) {
                 const messageData = {"messageText": receivedData.message, "sender":receivedData.sender }
                 chrome.runtime.sendMessage({ action: "messageForUser", event: messageData});
             }
-            if (receivedData.instruction === "welcomeBack") {
-                const messageData = {"messageText": receivedData.message}
-                chrome.runtime.sendMessage({ action: "welcomeBack", event: messageData});
-            }
+            // if (receivedData.instruction === "welcomeBack") {
+            //     const messageData = {"messageText": receivedData.message}
+            //     console.log("text: ", messageData)
+            //     chrome.runtime.sendMessage({ action: "welcomeBack", event: messageData});
+            // }
         };
     }
 }
 
 function receiveMessageFromOtherUser() {
-
+    
 }
 
 function sendMessageToOtherUser(message) {
@@ -137,7 +140,6 @@ function sendMessageToOtherUser(message) {
 
 socket.onmessage = function(event) {
     console.log("Message from server:", event.data);
-    // You can parse the message if it's in JSON format
     try {
         const message = JSON.parse(event.data);
         console.log("Parsed message:", message);
@@ -162,166 +164,4 @@ socket.onerror = function(error) {
 // Handle connection close
 socket.onclose = function(event) {
     console.log("Disconnected from the server.");
-=======
-console.log("service-worker.js loaded")
-
-const socket = new WebSocket('ws://localhost:8000');
-let userID = null
-let partner = null
-
-chrome.runtime.onMessage.addListener((message, event, sender, sendResponse) => {
-    if (message.action === "userSignIn") {
-        console.log("user signing in");
-        initiateOAuthFlow();
-    }
-    if (message.action === "userChoosePartner") {
-        console.log("user chose partner: ", message.event);
-        const partnerID = message.event;
-        checkPartner(partnerID);
-    }
-    if (message.action === "sendMessageToOtherUser") {
-        console.log("send this message: ", message.event);
-        const messageToSend = message.event;
-        sendMessageToOtherUser(messageToSend);
-    }
-})
-
-function initiateOAuthFlow() {
-    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-        console.log("token: ", token)
-        if (token === undefined) {
-        console.log("token error, exiting")
-        } else {
-            getUserId(token)
-        }
-    })
 }
-
-function getUserId(token) {
-    fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
-    headers: {
-        'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-    console.log('User ID:', data.email);
-    userID = data.email
-    })
-    .catch(error => {
-        console.error('Error fetching user info:', error);
-    });
-    checkUser()
-}
-
-function checkUser() {
-    console.log("server to check this user:", userID)
-    const messageToSend = {"userID": userID}
-    socket.send(JSON.stringify(messageToSend));
-    socket.onopen = function(event) {
-        console.log("open socket")
-    };
-    socket.onmessage = function(event) {
-        console.log(`Message from server: ${event.data}`);
-        if (event.data === "userAdded") {
-            chrome.runtime.sendMessage({ action: "showChoosePartner"});
-        }
-        if (event.data === "welcomeUserBack") {
-            chrome.runtime.sendMessage({ action: "welcomeUserBack"});
-        }
-    };
-}
-
-function checkPartner(partnerID) {
-    if (partnerID === null) {
-        console.log("no chosen partner, exiting")
-    } else {
-        console.log("check this partner: ", partnerID + "@gmail.com");
-        const checkUserAndPartner = {"userID": userID, "toID": partnerID + "@gmail.com"}
-        socket.send(JSON.stringify(checkUserAndPartner));
-        socket.onopen = function(event) {
-            console.log("open socket")
-        };
-        socket.onmessage = function(event) { 
-            
-            console.log(`Message from server: ${event.data}`);
-            if (event.data === "partnerAdded") {
-                chrome.runtime.sendMessage({ action: "showMessages"});
-                return;
-            }
-            if (event.data === "partnerIsInDb") {
-                chrome.runtime.sendMessage({ action: "partnerIsInDb"});
-                return;
-            }
-            if (event.data === "partnerIsNotInDb") {
-                chrome.runtime.sendMessage({ action: "partnerIsNotInDb"});
-                return;
-            }
-            if (event.data === "messageSent") {
-                chrome.runtime.sendMessage({ action: "messageSent"});
-                return;
-            }
-            const receivedData = JSON.parse(event.data);
-            console.log("received data", receivedData)
-            if (receivedData) {
-                console.log("valid data")
-            } else {
-                console.log("invalid data")
-            }
-            if (receivedData.instruction === "messageForUser") {
-                const messageData = {"messageText": receivedData.message, "sender":receivedData.sender }
-                chrome.runtime.sendMessage({ action: "messageForUser", event: messageData});
-            }
-        };
-    }
-}
-
-function receiveMessageFromOtherUser() {
-
-}
-
-function sendMessageToOtherUser(message) {
-    if (userID === null) {
-        console.log("no userID, exiting")
-    } else {
-        const messageToSend = {"userID": userID, "message": message}
-        console.log("messageToSend; ", messageToSend)
-        socket.send(JSON.stringify(messageToSend));
-        socket.onopen = function(event) {
-            console.log("open socket")
-        };
-        socket.onmessage = function(event) {
-            console.log(`Message from server: ${event.data}`);
-        };
-    }
-}
-
-socket.onmessage = function(event) {
-    console.log("Message from server:", event.data);
-    // You can parse the message if it's in JSON format
-    try {
-        const message = JSON.parse(event.data);
-        console.log("Parsed message:", message);
-    } catch(error) {
-        console.error("Error parsing message:", error);
-    }
-};
-
-// Connection opened
-socket.onopen = function(event) {
-    console.log("Connected to the server.");
-    // send message to the server once connection is open
-    const helloMsg = {"userID": userID, "message": "is connecting to server"};
-    socket.send(JSON.stringify(helloMsg));
-};
-
-// Listen for possible errors
-socket.onerror = function(error) {
-    console.error("WebSocket error:", error);
-};
-
-// Handle connection close
-socket.onclose = function(event) {
-    console.log("Disconnected from the server.");
->>>>>>> 5ccecd8d88ec9b841b77d6d1c1ff86a3cb06dc56
-};
